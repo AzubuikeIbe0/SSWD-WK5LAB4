6260703464b34edf397ed7ebc4e8b0f53bea2afa
ai-dev-official <ai.codespro@gmail.com>
2024-09-18 15:54:59 +0100

    before deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,57 +1,68 @@
-<nav>
-  <!-- Navigation menu with news sections - see CSS for layout-->
-<!-- Each has a data-source attribute which contains the URI to be requested-->
-<ul id="newsLinks" class="topnav">
-  <li><a id="Headlines" href="/" data-sveltekit-prefetch>Irish Times</a></li>
-  <li><a id="Headlines" href="/headlines" data-sveltekit-prefetch>Top Headlines</a></li>
-  <li><a id="Business" href="/business" data-sveltekit-prefetch>Business</a></li>
-  <li><a id="Sport" href="/sport" data-sveltekit-prefetch>Sport</a></li>
-</ul>
-</nav>
+<div class="header">
+  <div class="top">
+    <div class="menuIcon">
+      <i class='bx bx-menu'></i>
+      <i class='bx bx-search'></i>
+    </div>
+    <div class="topMenu">
+      <ul id="topMenu" class="topMenu">
+        <li><a id="Biafra" href="/" data-sveltekit-prefetch>Biafra</a></li>
+        <li><a id="International" href="/headlines" data-sveltekit-prefetch>International</a></li>
+        <li><a id="Canada" href="/business" data-sveltekit-prefetch>Canada</a></li>
+        <li><a id="Espanol" href="/sport" data-sveltekit-prefetch>Espanol</a></li>
+        <li><a id="中文" href="/sport" data-sveltekit-prefetch>中文</a></li>
+      </ul>
+    </div>
+    <div class="login">
+      <button class="subscribe">Subscribe for €0.50/week</button>
+      <button class="login">log in</button>
+    </div>
+  </div>
+  <div class="midLogo">
+    <div class="date">
+      <h6>Sunday, November 27, 2022</h6>
+      <small>Today's Paper</small>
+    </div>
+    <div class="logo">
+      <img src="./assets/img/btimes.png" alt="Irish Times Logo">
+    </div>
+    <div class="weather">
+     <div class="weather_details">
+      <i class='bx bxs-sun'></i>
+      <h4>9°C</h4>
+      <small>11° 5°</small>
+     </div>
+     <div class="market">
+      Nasdaq -0.52% <i class='bx bx-down-arrow-alt'></i>
+    </div>
+    </div>
+  </div>
+  <nav class="menu"> <!-- Navigation menu with news sections - see CSS for layout-->
+ <!-- Each has a data-source attribute which contains the URI to be requested-->
+    <ul id="newsLinks" class="topnav">
+      <li><a id="Headlines" href="/" data-sveltekit-prefetch>World</a></li>
+      <li><a id="Headlines" href="/headlines" data-sveltekit-prefetch>Biafra</a></li>
+      <li><a id="Business" href="/business" data-sveltekit-prefetch>Politics</a></li>
+      <li><a id="Sport" href="/sport" data-sveltekit-prefetch>Owerri</a></li>
+      <li><a href="#">Business</a></li>
+      <li><a href="#">Opinion</a></li>
+      <li><a href="#">Science</a></li>
+      <li><a href="#">Health</a></li>
+      <li><a href="#">Sports</a></li>
+      <li><a href="#">Arts</a></li>
+      <li><a href="#">Books</a></li>
+      <li><a href="#">Style</a></li>
+      <li><a href="#">Food</a></li>
+      <li><a href="#">Travel</a></li>
+      <li><a href="#">Magazine</a></li>
+      <li><a href="#">Cooking</a></li>
+      <li><a href="#">Bight</a></li>
+      <li><a href="#">Wirecutter</a></li>
+      <li><a href="#">Games</a></li>
+    </ul>
+    </nav>
+    <hr>
+</div>
+
 
 <slot />
-
-<style>
-  /*
-  /* https://www.w3schools.com/Css/tryit.asp?filename=trycss_navbar_horizontal_responsive
-  */
-  ul.topnav {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-  }
-
-  ul.topnav li {
-    float: left;
-  }
-
-  ul.topnav li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
-
-  ul.topnav li a:hover:not(.active) {
-    background-color: #111;
-  }
-
-  ul.topnav li a.active {
-    background-color: #04AA6D;
-  }
-
-  ul.topnav li.right {
-    float: right;
-  }
-
-  @media screen and (max-width: 600px) {
-
-    ul.topnav li.right,
-    ul.topnav li {
-      float: none;
-    }
-  }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,52 @@
 
 </script>
 
-<h2>Irish Times</h2>
+<!-- <h2>Irish Times</h2> -->
 <div id="stories" />
-<p>
-    Powered by <a href="https://newsapi.org/">https://newsapi.org/</a>
-</p>
-<div id="articles">
-    <!-- a Svelete for each to iterate through articles -->
-    {#each articles as article }
-    <article>
-        <h4>{article.title}</h4>
-        <p>{article.author}</p>
-        <!-- format the date using function -->
-        <p>{article.publishedAt}</p>
-        <img src={article.urlToImage} alt="caption">
-        <p>{article.description}</p>
-        <p><a href='{article.url}'>Read More</a></p>
-      </article>
-{/each}
+<div class="container">
+    <div id="articles">
+        <!-- a Svelete for each to iterate through articles -->
+        {#each articles as article }
+        <div class="front_page">
+            <div class="article_details">
+            <h2><a href='{article.url}'>{article.title}</a></h2>
+            <h6>{article.author}</h6>
+            <!-- format the date using function -->
+            <!-- <h6>{article.publishedAt}</h6> -->
+            <p>{article.description}</p>
+            <h6><a href='{article.url}'>Read More</a></h6>
+            </div>
+            <div class="article_img">
+                <a href='{article.url}'><img src={article.urlToImage} alt="caption"></a>
+            </div>
+          </div>
+    {/each}
+    </div>
+
+    <aside class="sidebar">
+        <h2>Top Stories</h2>
+        {#each articles as article }
+        
+            <div class="sidebar_details">
+            <h6>{article.author}</h6>
+            <h4><a href='{article.url}'>{article.title}</a></h4>
+            <!-- <p>{article.author}</p> -->
+            <!-- format the date using function -->
+            <!-- <p>{article.publishedAt}</p> -->
+         
+            
+            </div>
+            <div class="article_img">
+                <a href='{article.url}'><img src={article.urlToImage} alt="caption"></a>
+            </div>
+         
+    {/each}
+    </aside>
 </div>
+
+
+<footer>
+    <p>
+        Powered by <a href="https://newsapi.org/">https://newsapi.org/</a>
+    </p>
+</footer>
